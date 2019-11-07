7d3778d9ffcc9044e6ba6ffbbf0dd1e3dc4ba9ea
Nick Wylynko <nick1014375@gmail.com>
2019-11-07 17:30:05 +0800

    first build

---
+++ build/precache-manifest.91b37bc7e4c554e5e7170af2af59254b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0ff3bedf403a7a274a5f99fca46167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "378e166f757fbfe657da",
+    "url": "/static/css/main.3c564c59.chunk.css"
+  },
+  {
+    "revision": "2404963cea19f44f9799",
+    "url": "/static/js/2.1b2eccfe.chunk.js"
+  },
+  {
+    "revision": "378e166f757fbfe657da",
+    "url": "/static/js/main.201d407d.chunk.js"
+  },
+  {
+    "revision": "e64d01fb7871dba241e1",
+    "url": "/static/js/runtime-main.d130075c.js"
+  }
+]);
