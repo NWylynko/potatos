a3f36b7500a17c492728ee0d5cd131259a7815ab
Nick Wylynko <nick1014375@gmail.com>
2019-11-07 19:41:46 +0800

    gucci

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
 import React from 'react';
 import './App.css';
+import { games, list } from './games';
 
 export default function App() {
+
+  // current game from list
+  const [cg_id, set_cg] = React.useState()
+
+  console.log(cg_id)
+
+  /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  const range = n => [...Array(n).keys()]
+
+  const dropdownlist = range(14).map((item) =>
+  <div style={{flexDirection: 'row'}}>
+    <button onClick={() => {set_cg(item * 5 + 0)}}>{list[item * 5 + 0].name}</button>
+    <button onClick={() => {set_cg(item * 5 + 1)}}>{list[item * 5 + 1].name}</button>
+    <button onClick={() => {set_cg(item * 5 + 2)}}>{list[item * 5 + 2].name}</button>
+    <button onClick={() => {set_cg(item * 5 + 3)}}>{list[item * 5 + 3].name}</button>
+    <button onClick={() => {set_cg(item * 5 + 4)}}>{list[item * 5 + 4].name}</button>
+  </div>
+    
+  );
+
   return (
     <div className="App">
       <h1>bored in class</h1>
+
+      <div className="dropdown">
+        <button onClick={myFunction} className="dropbtn">{cg_id ? list[cg_id].name : 'select a game'}</button>
+        <div id="myDropdown" className="dropdown-content">
+          {dropdownlist}
+        </div>
+      </div>
+
+      <Game game={cg_id ? games[list[cg_id].game] : null}/>
+      
+      
     </div>
   );
+}
+
+function Game({game}) {
+  if (game) {
+    return (
+      <object width="100%" height="100%"
+        data={game} type="application/x-shockwave-flash">
+        <param name="quality" value="autohigh" />
+        <param name="allowFullScreen" value="true" />
+        <param name="wmode" value="direct" />
+        <param name="allowScriptAccess" value="always" />
+        <param name="scale" value="showall" />
+        <param name="bgcolor" value="red" />
+        <param name="hasPriority" value="true" />
+        <param value="internal" name="allownetworking" />
+      </object>
+    )
+  } else {
+    return (null)
+  }
 }
---
+++ src/games.js
@@ -0,0 +1,444 @@
+import papaspancakeria from "./swf/papaspancakeria.swf";
+import warfare19172267 from "./swf/warfare-1917-2267.swf";
+import bloonstowerdefense5 from "./swf/bloons-tower-defense-5.swf";
+import tetris from "./swf/tetris.swf";
+import monopoly2 from "./swf/monopoly2.swf";
+import papasscooperia_v102 from "./swf/papasscooperia_v102.swf";
+import papassushiria from "./swf/papassushiria.swf";
+import caribbeanadmiral from "./swf/caribbeanadmiral.swf";
+import papasburgeria from "./swf/papasburgeria.swf";
+import bubbletanks2 from "./swf/BubbleTanks2.swf";
+import papaspizzeria_v2 from "./swf/papaspizzeria_v2.swf";
+import ptd3 from "./swf/ptd3.swf";
+import bubble_tanks from "./swf/bubble_tanks.swf";
+import pokemontowerdefense2 from "./swf/PokemonTowerDefense2.swf";
+import papalouie_v2 from "./swf/papalouie_v2.swf";
+import supermario63 from "./swf/SuperMario63.swf";
+import oneononehockey from "./swf/one-on-one-hockey.swf";
+import papashotdoggeria from "./swf/papashotdoggeria.swf";
+import airportmadness4 from "./swf/airport-madness-4.swf";
+import learntofly3 from "./swf/learn-to-fly-3.swf";
+import civilizationswars from "./swf/civilizations-wars.swf";
+import papasfreezeria from "./swf/papasfreezeria.swf";
+import sportsheadsbasketballchampionship from "./swf/sports-heads-basketball-championship.swf";
+import jacksmith from "./swf/jacksmith.swf";
+import feudalismii2581 from "./swf/feudalism-ii-2581.swf";
+import civilizationswars2prime from "./swf/civilizations-wars-2-prime.swf";
+import bloonstowerdefence3 from "./swf/bloons-tower-defence-3.swf";
+import pvz from "./swf/pvz.swf";
+import monopoly from "./swf/monopoly.swf";
+import smc from "./swf/smc.swf";
+import worldwars2 from "./swf/world-wars-2.swf";
+import learntofly2 from "./swf/learn-to-fly-2.swf";
+import snake from "./swf/snake.swf";
+import gba from "./swf/gba.swf";
+import nesbox from "./swf/Nesbox.swf";
+import tboi from "./swf/tboi.swf";
+import dinorun from "./swf/dino-run.swf";
+import papasdonuteria from "./swf/papasdonuteria.swf";
+import flight from "./swf/flight.swf";
+import raze2 from "./swf/raze2.swf";
+import run3online from "./swf/run3online.swf";
+import raze from "./swf/Raze.swf";
+import chess from "./swf/Chess.swf";
+import stickwar214346 from "./swf/stick-war-2-14346.swf";
+import oneononesoccer from "./swf/one-on-one-soccer.swf";
+import papaswingeria from "./swf/papaswingeria.swf";
+import papascheeseria_102 from "./swf/papascheeseria_102.swf";
+import gb from "./swf/gb.swf";
+import motox3m1 from "./swf/moto-x3m-1.swf";
+import papaspastaria from "./swf/papaspastaria.swf";
+import sega from "./swf/sega.swf";
+import nes from "./swf/nes.swf";
+import feudalism2 from "./swf/feudalism-2.swf";
+import papastacomia from "./swf/papastacomia.swf";
+import kgrmzuot4z from "./swf/kgrMZuOt4z.swf";
+import bubbletankstowerdefense from "./swf/bubble-tanks-tower-defense.swf";
+import dl3 from "./swf/dl3.swf";
+import tt2 from "./swf/tt2.swf";
+import superfightersgame from "./swf/superfighters-game.swf";
+import gunmayhem2morema13824 from "./swf/gun-mayhem-2-more-ma-13824.swf";
+import awesome_tanks_2 from "./swf/awesome_tanks_2.swf";
+import papascupcakeria from "./swf/papascupcakeria.swf";
+import bt2h from "./swf/bt2h.swf";
+import btd5dat from "./swf/btd5-dat.swf";
+import btd5hacked from "./swf/btd5hacked.swf";
+import hexempire from "./swf/hex-empire.swf";
+import oneononefootball from "./swf/one-on-one-football.swf";
+import papasbakeria from "./swf/papasbakeria.swf";
+import sushicat from "./swf/SushiCat.swf";
+import battleship from "./swf/Battleship.swf";
+import snes from "./swf/snes.swf";
+import aquaboy from "./swf/aqua-boy.swf";
+import strikeforceheroes2 from "./swf/Strike Force Heroes 2.swf";
+
+export const games = {
+  papaspancakeria,
+  warfare19172267,
+  bloonstowerdefense5,
+  tetris,
+  monopoly2,
+  papasscooperia_v102,
+  papassushiria,
+  caribbeanadmiral,
+  papasburgeria,
+  bubbletanks2,
+  papaspizzeria_v2,
+  ptd3,
+  bubble_tanks,
+  pokemontowerdefense2,
+  papalouie_v2,
+  supermario63,
+  oneononehockey,
+  papashotdoggeria,
+  airportmadness4,
+  learntofly3,
+  civilizationswars,
+  papasfreezeria,
+  sportsheadsbasketballchampionship,
+  jacksmith,
+  feudalismii2581,
+  civilizationswars2prime,
+  bloonstowerdefence3,
+  pvz,
+  monopoly,
+  smc,
+  worldwars2,
+  learntofly2,
+  snake,
+  gba,
+  nesbox,
+  tboi,
+  dinorun,
+  papasdonuteria,
+  flight,
+  raze2,
+  run3online,
+  raze,
+  chess,
+  stickwar214346,
+  oneononesoccer,
+  papaswingeria,
+  papascheeseria_102,
+  gb,
+  motox3m1,
+  papaspastaria,
+  sega,
+  nes,
+  feudalism2,
+  papastacomia,
+  kgrmzuot4z,
+  bubbletankstowerdefense,
+  dl3,
+  tt2,
+  superfightersgame,
+  gunmayhem2morema13824,
+  awesome_tanks_2,
+  papascupcakeria,
+  bt2h,
+  btd5dat,
+  btd5hacked,
+  hexempire,
+  oneononefootball,
+  papasbakeria,
+  sushicat,
+  battleship,
+  snes,
+  aquaboy,
+  strikeforceheroes2
+}
+
+export const list = [{
+    name: "papaspancakeria",
+    game: "papaspancakeria"
+  },
+  {
+    name: "warfare19172267",
+    game: "warfare19172267"
+  },
+  {
+    name: "bloonstowerdefense5",
+    game: "bloonstowerdefense5"
+  },
+  {
+    name: "tetris",
+    game: "tetris"
+  },
+  {
+    name: "monopoly2",
+    game: "monopoly2"
+  },
+  {
+    name: "papasscooperia_v102",
+    game: "papasscooperia_v102"
+  },
+  {
+    name: "papassushiria",
+    game: "papassushiria"
+  },
+  {
+    name: "caribbeanadmiral",
+    game: "caribbeanadmiral"
+  },
+  {
+    name: "papasburgeria",
+    game: "papasburgeria"
+  },
+  {
+    name: "bubbletanks2",
+    game: "bubbletanks2"
+  },
+  {
+    name: "papaspizzeria_v2",
+    game: "papaspizzeria_v2"
+  },
+  {
+    name: "ptd3",
+    game: "ptd3"
+  },
+  {
+    name: "bubble_tanks",
+    game: "bubble_tanks"
+  },
+  {
+    name: "pokemontowerdefense2",
+    game: "pokemontowerdefense2"
+  },
+  {
+    name: "papalouie_v2",
+    game: "papalouie_v2"
+  },
+  {
+    name: "supermario63",
+    game: "supermario63"
+  },
+  {
+    name: "oneononehockey",
+    game: "oneononehockey"
+  },
+  {
+    name: "papashotdoggeria",
+    game: "papashotdoggeria"
+  },
+  {
+    name: "airportmadness4",
+    game: "airportmadness4"
+  },
+  {
+    name: "learntofly3",
+    game: "learntofly3"
+  },
+  {
+    name: "civilizationswars",
+    game: "civilizationswars"
+  },
+  {
+    name: "papasfreezeria",
+    game: "papasfreezeria"
+  },
+  {
+    name: "sportsheadsbasketballchampionship",
+    game: "sportsheadsbasketballchampionship"
+  },
+  {
+    name: "jacksmith",
+    game: "jacksmith"
+  },
+  {
+    name: "feudalismii2581",
+    game: "feudalismii2581"
+  },
+  {
+    name: "civilizationswars2prime",
+    game: "civilizationswars2prime"
+  },
+  {
+    name: "bloonstowerdefence3",
+    game: "bloonstowerdefence3"
+  },
+  {
+    name: "pvz",
+    game: "pvz"
+  },
+  {
+    name: "monopoly",
+    game: "monopoly"
+  },
+  {
+    name: "smc",
+    game: "smc"
+  },
+  {
+    name: "worldwars2",
+    game: "worldwars2"
+  },
+  {
+    name: "learntofly2",
+    game: "learntofly2"
+  },
+  {
+    name: "snake",
+    game: "snake"
+  },
+  {
+    name: "gba",
+    game: "gba"
+  },
+  {
+    name: "nesbox",
+    game: "nesbox"
+  },
+  {
+    name: "tboi",
+    game: "tboi"
+  },
+  {
+    name: "dinorun",
+    game: "dinorun"
+  },
+  {
+    name: "papasdonuteria",
+    game: "papasdonuteria"
+  },
+  {
+    name: "flight",
+    game: "flight"
+  },
+  {
+    name: "raze2",
+    game: "raze2"
+  },
+  {
+    name: "run3online",
+    game: "run3online"
+  },
+  {
+    name: "raze",
+    game: "raze"
+  },
+  {
+    name: "chess",
+    game: "chess"
+  },
+  {
+    name: "stickwar214346",
+    game: "stickwar214346"
+  },
+  {
+    name: "oneononesoccer",
+    game: "oneononesoccer"
+  },
+  {
+    name: "papaswingeria",
+    game: "papaswingeria"
+  },
+  {
+    name: "papascheeseria_102",
+    game: "papascheeseria_102"
+  },
+  {
+    name: "gb",
+    game: "gb"
+  },
+  {
+    name: "motox3m1",
+    game: "motox3m1"
+  },
+  {
+    name: "papaspastaria",
+    game: "papaspastaria"
+  },
+  {
+    name: "sega",
+    game: "sega"
+  },
+  {
+    name: "nes",
+    game: "nes"
+  },
+  {
+    name: "feudalism2",
+    game: "feudalism2"
+  },
+  {
+    name: "papastacomia",
+    game: "papastacomia"
+  },
+  {
+    name: "kgrmzuot4z",
+    game: "kgrmzuot4z"
+  },
+  {
+    name: "bubbletankstowerdefense",
+    game: "bubbletankstowerdefense"
+  },
+  {
+    name: "dl3",
+    game: "dl3"
+  },
+  {
+    name: "tt2",
+    game: "tt2"
+  },
+  {
+    name: "superfightersgame",
+    game: "superfightersgame"
+  },
+  {
+    name: "gunmayhem2morema13824",
+    game: "gunmayhem2morema13824"
+  },
+  {
+    name: "awesome_tanks_2",
+    game: "awesome_tanks_2"
+  },
+  {
+    name: "papascupcakeria",
+    game: "papascupcakeria"
+  },
+  {
+    name: "bt2h",
+    game: "bt2h"
+  },
+  {
+    name: "btd5dat",
+    game: "btd5dat"
+  },
+  {
+    name: "btd5hacked",
+    game: "btd5hacked"
+  },
+  {
+    name: "hexempire",
+    game: "hexempire"
+  },
+  {
+    name: "oneononefootball",
+    game: "oneononefootball"
+  },
+  {
+    name: "papasbakeria",
+    game: "papasbakeria"
+  },
+  {
+    name: "sushicat",
+    game: "sushicat"
+  },
+  {
+    name: "battleship",
+    game: "battleship"
+  },
+  {
+    name: "snes",
+    game: "snes"
+  },
+  {
+    name: "aquaboy",
+    game: "aquaboy"
+  },
+  {
+    name: "strikeforceheroes2",
+    game: "strikeforceheroes2"
+  }
+
+]
